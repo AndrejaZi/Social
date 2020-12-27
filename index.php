95d197ee3c6d39481864eeb3e25a0ee890ce7340
AndrejaZi <andrejaziv1997@gmail.com>
2020-12-27 17:46:10 +0100

    Ubacene osnovne funkcije za php

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+if(isset($_POST['submit'])){
+    $name = $_POST['name']; //name
+    $name = strip_tags($name); //removing special chars
+    $name = ucfirst(strtolower($name)); //seting name to be first letter capital
+
+    $email = $_POST['email']; //email
+    $email = strip_tags($email);
+    $email = ucfirst(strtolower($email));
+
+    $emailVerify = $_POST['email2']; //For email verification
+    $emailVerify = strip_tags($emailVerify);
+    $emailVerify = ucfirst(strtolower($emailVerify));
+
+    $pass = $_POST['pass']; //password
+
+    $passVerify = $_POST['pass2']; //For password verification
+
+    $date = date("d-m-yy"); // Current date
+    
+
+}
+    
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +32,20 @@
     <title>Document</title>
 </head>
 <body>
-    <p>This is only the beggining</p>
-    <p>added</p>
+    
+    <form action="index.php" method="POST">
+        <input type="text" name="name" placeholder="Name">
+        <br>
+        <input type="text" name="email" placeholder="Email">
+        <br>
+        <input type="text" name="email2" placeholder="Verify Email">
+        <br>
+        <input type="password" name="pass" placeholder="Password">
+        <br>
+        <input type="password" name="pass2" placeholder="Verify Password">
+        
+        <br>
+        <input type="submit" name="submit" value="Registruj se">
+    </form>
 </body>
 </html>
