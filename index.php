0c06a8fd413543f171baf6dc75f7f674de191f42
AndrejaZi <andrejaziv1997@gmail.com>
2021-01-28 19:25:07 +0100

    AJAX Call

--- index.php
+++ index.php
@@ -68,7 +68,15 @@
                     document.getElementById('suggest').innerHTML = '';
                 }else{
                     //ajax request
-                    
+                    let xml = new XMLHttpRequest();
+                    xml.onreadystatechange = function(){
+                        if(this.readyState === 4 && this.status === 200){
+                            document.getElementById('suggest').innerHTML = this.responseText;
+                        }
+                    }
+
+                    xml.open("GET", "names.php?q=" + value, true);
+                    xml.send();
                 }
             }
         //console.log(x);
--- names.php
+++ names.php
@@ -1,25 +1,48 @@
-$people = ["Andreja"];
-$people = ["Marko"];
-$people = ["Svetozar"];
-$people = ["Milenko"];
-$people = ["Branko"];
-$people = ["Vanja"];
-$people = ["Gordana"];
-$people = ["Dragana"];
-$people = ["Djordje"];
-$people = ["Elizabeta"];
-$people = ["Zivojin"];
-$people = ["Ilija"];
-$people = ["Jelena"];
-$people = ["Lenka"];
-$people = ["Kostarika"];
-$people = ["Ujka"];
-$people = ["Zabac"];
-$people = ["Toma"];
+<?php
+
+
+$people[] = "Andreja";
+$people[] = "Marko";
+$people[] = "Svetozar";
+$people[] = "Milenko";
+$people[] = "Branko";
+$people[] = "Vanja";
+$people[] = "Gordana";
+$people[] = "Dragana";
+$people[] = "Djordje";
+$people[] = "Elizabeta";
+$people[] = "Zivojin";
+$people[] = "Ilija";
+$people[] = "Jelena";
+$people[] = "Lenka";
+$people[] = "Kostarika";
+$people[] = "Smiljka";
+$people[] = "Anastasija";
+$people[] = "Irena";
+$people[] = "Goca";
+$people[] = "Valerija";
 
 $query = $_REQUEST['q'];
 
-<!-- Izlazna informacija -->
-$suggestion = '';
-<!-- len se koristi za proveru -->
-$len = strlen($query); 
+// Izlazna informacija 
+$suggestion = "";
+
+if($query !== ""){
+    $query = strtolower($query);
+    //  len se koristi za proveru 
+    $len = strlen($query); 
+    foreach($people as $person){
+        if(stristr($query, substr($person, 0, $len))){
+           
+           if($suggestion === ""){
+                $suggestion = $person;
+           }else{
+                $suggestion .= ", " . $person;
+           }
+            
+        }
+    }
+
+}
+
+echo $suggestion === "" ? 'Nema predloga' : $suggestion;
