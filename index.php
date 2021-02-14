80a1d538c9e229b6c22c4f9c09c476b42e313e2d
AndrejaZi <andrejaziv1997@gmail.com>
2021-02-14 18:23:03 +0100

    Added custom username input by user. Added username validation. Added Insertion in database after every check is passed

--- index.php
+++ index.php
@@ -2,39 +2,42 @@
     session_start();
 
     $con = mysqli_connect("localhost", "root", "", "test");
+    
 
     if(filter_has_var(INPUT_POST, 'submit')){
         $fName = htmlentities($_POST['fName']);
         $fName = str_replace(" ", "", $fName);
         $fName = ucfirst(strtolower($fName)); 
-        $_SESSION['reg_fName'] = $fName;
 
         $lName = htmlentities ($_POST['lName']);
         $lName = str_replace(" ", "", $lName);
         $lName = ucfirst(strtolower($lName)); 
-        $_SESSION['reg_lName'] = $lName;
-        
+
+        $username = htmlentities($_POST['username']);
+
         $email = htmlentities($_POST['email']);
         $email = str_replace(" ", "", $email);
         $email = ucfirst(strtolower($email)); 
-        $_SESSION['reg_email'] = $email;
 
         $chechEmail = htmlentities($_POST['emailCheck']);
         $chechEmail = str_replace(" ", "", $chechEmail);
         $chechEmail = ucfirst(strtolower($chechEmail));
-        $_SESSION['reg_checkEmail'] = $chechEmail;
 
         $pass = strip_tags($_POST['pass']);
         $passVerify = strip_tags($_POST['passVerify']);
-        $date = date("d-m-Y");
+
+        //Used as boolean to give user feedback if registration was succesful or not
+        $registerSuccess;
+        
         //echo $fName . " " . $lName . " " . $email . " " . $chechEmail . " " . $pass . " " . $passVerify . " " . $date;
 
+        //to be used for user errors i.e. wrong password, email in use...
         $errorArray = array();
 
+        //Email check
         if($email == $chechEmail){
             if(filter_var($email, FILTER_VALIDATE_EMAIL)){
                 
-
                 //Chech if email exists in database
                 $checkEmail = mysqli_query($con, "SELECT email FROM users WHERE email='$email'");
 
@@ -43,35 +46,86 @@
                 if($numOfRows > 0){
                     array_push($errorArray, "Email postoji u bazi </br>"); 
                 }else{
-                    //Email is ok
+                    //Email is ok and stored in session
+                    $_SESSION['reg_email'] = $email;
                 }
+            }else{
+                //Email is not valid format
+                array_push($errorArray, "Email nije vlidnog formata </br>");
             }   
 
         }else{
-            array_push($errorArray, "Email nije vlidnog formata </br>");
+            //email do not match
+            array_push($errorArray, "Mejlovi se ne podudaraju");
         }
 
+        //name check
+        if(strlen($fName) > 50 || strlen($fName) <= 1){
+            array_push($errorArray, "Vase ime mora bit izmedju 2 i 50 karaktera </br>");
+        }else{
+            $_SESSION['reg_fName'] = $fName;
+        }
+
+        //Last name check
+        if(strlen($lName) > 50 || strlen($lName) <= 1){
+            array_push($errorArray, "Vase prezime mora bit izmedju 2 i 50 karaktera </br>");
+        }else{
+            $_SESSION['reg_lName'] = $lName;
+        }
+
+        //Username check
+        if(strlen($username) > 50 || strlen($username) <= 3){
+            array_push($errorArray, "Korisnicko ime ne sme biti duze od 50 karaktera niti manje od 3</br>");
+        }else{
+
+            //check if username is in database
+            $usernameQuery = mysqli_query($con, "SELECT username FROM users where username='$username'");
+            $userNameQueryResult = mysqli_num_rows($usernameQuery);
+            if($userNameQueryResult > 0){
+                //username in use
+                array_push($errorArray, "Korisnicko ime postoji");
+            }else{
+                //username is free to use
+                $_SESSION['reg_username'] = $username;
+
+            }
+        }
+
+        //Password check
         if($pass != $passVerify){
             array_push($errorArray,"Lozinke se ne podudaraju </br>" );
         }
 
-        if(strlen($fName) > 50 || strlen($fName) <= 1){
-            array_push($errorArray, "Vase ime mora bit izmedju 2 i 50 karaktera </br>");
-        }
-        if(strlen($lName) > 50 || strlen($lName) <= 1){
-            array_push($errorArray, "Vase prezime mora bit izmedju 2 i 50 karaktera </br>");
-        }
-
-        //Ovo sto je cudno proverava da li se lozinka slaze sa nekim standardom, tj da li ima slovo, broj i neki od znakova, takodje gleda da li je manje od 8 ili vise od 50 karakatera
-        if(preg_match("/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z]{8,50}$/", $pass)){
-            
-        }else{
+        //Check if user password is in certian parameters, if it has at least one letter, one number and if it's between 8 and 50 characters long
+        if(!preg_match("/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z]{8,50}$/", $pass)){
             array_push($errorArray, "Lozinka mora sadrzati bar jedno slovo, broj i mora imati bar 8 karaktera </br>");
         }
+ 
         
+        if(empty($errorArray)){
+           //######################################
+           //#######  SUCCESFUL DATA CHECK  #######
+           //######################################
+            //hashing password
+            $pass = password_hash($pass, PASSWORD_BCRYPT);
 
-        //echo $_SESSION["reg_fName"] . " " . $_SESSION["reg_lName"];
-        echo implode($errorArray);
+            //Used for default profile pic they are labeled from 1-11
+            $rand = rand(1, 11);
+            
+            $profilePic = "Assets/profile_pics/defaults/pic" . $rand;
+            $date = date("Y-m-d");
+            $insertDataQuery = mysqli_query($con, "INSERT INTO users VALUES('', '$fName', '$lName', '$username', '$email', '$pass', '$date', '$profilePic', '0', '0', 'no', ',')");
+     
+
+            // if($insertDataQuery == true){
+            //     $registerSuccess = true;
+            //     echo $registerSuccess;
+            // }else{
+            //     $registerSuccess = false;
+            //     echo $registerSuccess;
+            // }
+
+        }
     }
 ?>
 
@@ -81,15 +135,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Assets/css/style.css">
     <title>Ucenjeeee</title>
 </head>
 <body>
     <div class="container">
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-            <h3>Email validation</h3>
+            <h3>Register</h3>
             <input type="text" class="input inputFname" placeholder = "Enter name" name="fName" value="<?php echo isset($_POST['fName']) ? $fName : ''; ?>">
             <input type="text" class="input inputLname" placeholder = "Enter name" name="lName" value="<?php echo isset($_POST['lName']) ? $lName : ''; ?>">
+            <input type="text" class="input inputUsername" placeholder="Choose username" name="username" value="<?php echo isset($_POST['username']) ? $username : '' ?>">
             <input type="text" class="input email" placeholder="Enter email" name="email" value="<?php echo isset($_POST['lName']) ? $email : ''; ?>">
             <input type="text" class="input emailCheck" placeholder="Enter email again" name="emailCheck" value="<?php echo isset($_POST['lName']) ? $chechEmail  : ''; ?>">
             <input type="password" class="input password" placeholder="Password" name="pass">
@@ -98,8 +153,15 @@
             <?php if(!empty($errorArray)){
                 echo "<p class='error'>." . implode($errorArray) . "</p>";
             } ?>
+            <?php if(filter_has_var(INPUT_POST, 'registerSuccess')){
+                    if($registerSuccess === true){
+                        echo "<p class='regSuccess'>Registration was succesfull</p>";
+                    }elseif($registerSuccess === false){
+                        echo "<p class='regFail'>Registration was not successful</p>";
+                    }
+            } ?>
         </form>
     </div>
-    <script src="app.js"></script>
+    <script src="Assets/js/app.js"></script>
 </body>
 </html>
